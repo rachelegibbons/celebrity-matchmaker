36b9bfa8e0dcded32106aebec7258e5007ee750a
RG <rachelegibbons@gmail.com>
2017-02-21 21:11:03 -0800

    updated to show celebrity matches depending on input

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,28 @@
 $(document).ready(function(){
-  $("form#matchmaker").submit(function(){
+  $("form#matchmaker").submit(function(event){
+    event.preventDefault();
+    var name = $("input#nameInput").val();
+    var age = parseInt($("input#ageInput").val());
+    var dob = $("input#dobInput").val();
+    var gender = $("select#genderInput").val();
+    var sexuality = $("select#sexualityInput").val();
+    var zodiac = $("select#zodiacInput").val();
+    var color = $("input#colorInput").val();
 
-      var name = $("input#nameInput").val();
-      var age = parseInt($("input#ageInput").val());
-      var dob = parseInt($("input#dobInput").val());
-      var gender = $("#genderInput").val();
-      var sexuality = $("#sexualityInput").val();
-      var zodiac = $("#zodiacInput").val();
-      var color = $("input#colorInput").val());
+    $(".name").text(name);
 
-      $(".name").append(name);
-      $(".age").append(age);
-      $(".dob").append(dob);
-      $(".gender").append(gender);
-      $(".sexuality").append(sexuality);
-      $(".zodiac").append(zodiac);
-      $(".color").append(color);
+    if (gender === "male" && sexuality === "gay") {
+      $("form").hide();
+      $("#neilPatrickHarris").show();
+    } else if (gender === 'female' && sexuality === 'lesbian') {
+        $("#shane").show();
+    } else if (gender === 'female' && sexuality === 'straight') {
+      $("#jeffGoldblum").show();
+    } else if (gender === 'male' && sexuality === 'straight') {
+      $("#drewBarrymore").show();
+    } else {
+      $("#noResult").show();
+    }
 
   });
 
